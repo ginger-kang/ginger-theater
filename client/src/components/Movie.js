ad3993eab3acfee8f05baa9d89ae93b6e34855b3
ginger-kang <kdhoon07@gmail.com>
2020-05-04 14:21:48 +0900

    poster

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,12 +4,7 @@ import './Movie.css';
 import { API_URL, API_KEY, IMAGE_BASE_URL } from './config';
 function Movie({id, title, image}) {
     return (
-    <div className="movie">
-        <img src={`${API_URL}/movie/28/images?api_key=${API_KEY}&language=en-US`} alt={title} title={title}/>
-        <div className="nav_bar">
-            <span>W / N</span>
-        </div>
-    </div>
+        <img src={`${IMAGE_BASE_URL}w154/${image}`} alt={title} title={title}/>
     );
 }
 
--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,3 +1,3 @@
 export const API_URL = `https://api.themoviedb.org/3`;
 export const API_KEY = `048a1358d5e30b00877a844ed99b466f`;
-export const IMAGE_BASE_URL = `http://image.tmdb.org/t/p/`;
+export const IMAGE_BASE_URL = `https://image.tmdb.org/t/p/`;
