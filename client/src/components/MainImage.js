c73bfdbffa02625e34e3fd754b79e3bb2fecc3c1
ginger-kang <kdhoon07@gmail.com>
2020-05-06 00:22:27 +0900

    modifydesign

--- client/src/components/MainImage.js
+++ client/src/components/MainImage.js
@@ -7,13 +7,14 @@ const MainImage = ({props, image}) => {
             background: `url('${image}')`,
             width: '100%',
             height: '100%',
-            backgroundSize: '100%, cover',
+            backgroundSize: 'cover',
             backgroundPosition: 'center, center',
+            backgroundRepeat: 'no-repeat',
             position: 'relative'
         }} className="mainImage">
             <div className="mainContext">
                 <h1>{props.original_title}</h1>
-                {/* <p style={{textOverflow: 'ellipsis'}}>{props.overview}</p> */}
+                <p style={{textOverflow: 'ellipsis'}}>{props.overview}</p>
             </div>
         </div>
     );
