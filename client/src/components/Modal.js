0f7de2a52f5fb1c1d10864d5cc583ba242653763
ginger-kang <kdhoon07@gmail.com>
2020-05-06 22:21:18 +0900

    modalDesign

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -6,15 +6,37 @@ const Modal = ({title, image, content, show, handleClose}) => {
 
     return (
         show ?
-        <div className={showHideModalContainer} onClick={handleClose}>
-            <div className="modalMain">
-                <img src={`${IMAGE_BASE_URL}w500/${image}`} alt={title}/>
-                <div className="modalText">
-                    <h2>{title}</h2>
-                    <p>{content}</p>
+            <div className={showHideModalContainer} onClick={handleClose} style={{
+                background: `url('${IMAGE_BASE_URL}original/${image}')`,
+                zIndex: '300',
+                width: '70%',
+                height: '85%',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center, center',
+                backgroundRepeat: 'no-repeat',
+                position: 'fixed',
+                top: '5%',
+                left: '15%',
+                bottom: '3rem',
+                margin: '0 0rem',
+                opacity: '0.9' 
+            }}>
+                <div className="modalText" style={{
+                    position: 'absolute',
+                    left: '3rem',
+                    bottom: '11rem',
+                    width: 'auto',
+                    height: '3rem',
+                    textOverflow: 'ellipsis'
+                }}>
+                    <h1 style={{
+                        fontSize: '3rem'
+                    }}>{title}</h1>
+                    <p style={{
+                        fontSize: '1.3rem',
+                    }}>{content}</p>
                 </div>
             </div>
-        </div>
         : null
     );
 }
