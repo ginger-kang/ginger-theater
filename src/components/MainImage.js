18a9e7ff51e7d9f16b81a9735a143379528b436c
ginger-kang <kdhoon07@gmail.com>
2020-06-17 21:03:20 +0900

    fix: background shorthand conflict

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,48 +1,54 @@
-import React, { useState } from 'react';
-import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
-import { IMAGE_BASE_URL } from './config';
-import './MainImage.css'
+import React, { useState } from "react";
+import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import { IMAGE_BASE_URL } from "./config";
+import "./MainImage.css";
 
-const MainImage = ({props}) => {
-    const [ imageIndex, setImageindex ] = useState(0);
+const MainImage = ({ props }) => {
+  const [imageIndex, setImageindex] = useState(0);
 
-    const imageLeftSliding = () => {
-        if ( imageIndex === 0) {
-            setImageindex(4);
-        }else {
-            setImageindex( imageIndex - 1 );
-        }
-        console.log(imageIndex);
+  const imageLeftSliding = () => {
+    if (imageIndex === 0) {
+      setImageindex(4);
+    } else {
+      setImageindex(imageIndex - 1);
     }
+    console.log(imageIndex);
+  };
 
-    const imageRightSliding = () => {
-        if ( imageIndex === 4) {
-            setImageindex(0);
-        }else {
-            setImageindex( imageIndex + 1 );
-        }
-        console.log(imageIndex);
+  const imageRightSliding = () => {
+    if (imageIndex === 4) {
+      setImageindex(0);
+    } else {
+      setImageindex(imageIndex + 1);
     }
+    console.log(imageIndex);
+  };
 
-    return (
-        <>
-            <div style = {{
-                background: `url('${IMAGE_BASE_URL}original${props[imageIndex].poster_path}')`,
-                width: '100%',
-                height: '100vh',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center, center',
-                backgroundRepeat: 'no-repeat'
-            }} className="mainImage">
-            </div>
-            <button className="leftButton" onClick={imageLeftSliding}><AiOutlineLeft size={40}/></button>
-            <button className="rightButton" onClick={imageRightSliding}><AiOutlineRight size={40}/></button>
-            <div className="mainContext">
-                <h1>{props[imageIndex].original_title}</h1>
-                <p>{props[imageIndex].overview}</p>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url('${IMAGE_BASE_URL}original${props[imageIndex].backdrop_path}')`,
+          width: "100%",
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center, center",
+          backgroundRepeat: "no-repeat",
+        }}
+        className="mainImage"
+      ></div>
+      <button className="leftButton" onClick={imageLeftSliding}>
+        <AiOutlineLeft size={40} />
+      </button>
+      <button className="rightButton" onClick={imageRightSliding}>
+        <AiOutlineRight size={40} />
+      </button>
+      <div className="mainContext">
+        <h1>{props[imageIndex].original_title}</h1>
+        <p>{props[imageIndex].overview}</p>
+      </div>
+    </>
+  );
+};
 
 export default MainImage;
--- src/containers/SearchMovie.js
+++ src/containers/SearchMovie.js
@@ -1,47 +1,58 @@
-import React, { useState } from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { API_KEY, API_URL } from '../components/config';
-import axios from 'axios';
-import Search from '../components/Search';
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { API_KEY, API_URL } from "../components/config";
+import axios from "axios";
+import Search from "../components/Search";
 
 const SearchMovie = (props) => {
-    const [inputValue, setInputValue] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-    //const [ isImage, setisImage ] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  //const [ isImage, setisImage ] = useState(null);
 
-    const getSearchMovies = async () => {
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&sort_by=popularity&language=en-US&query=${inputValue}&include_adult=false`);
-        const searchData = response.data.results || [];
-        //console.log(searchResults);
-        setSearchResults(searchData);
-        //console.log(searchData[0].poster_path)
-    }
-
-    const handleChange = (e) => {
-        setInputValue(e.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        getSearchMovies();
-        setInputValue('');
-    }
-    
-    return (
-        <div className='search'>
-            <div className='inputContainer'>
-                <span className='inputIcon'><AiOutlineSearch size={40}/></span>
-                <form className="inputForm" onSubmit={handleSubmit}>
-                    <input className='inputText' type='text' placeholder='Search Movie...' value={inputValue || ''} onChange={handleChange}/>
-                </form>
-            </div>
-            <div className='searchContainer'>
-                {searchResults && searchResults.map(movie => (
-                    <Search props={movie} image={movie.poster_path} key={movie.id}/>
-                ))}
-            </div>
-        </div>
+  const getSearchMovies = async () => {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&sort_by=popularity&language=en-US&query=${inputValue}&include_adult=false`
     );
-}
+    const searchData = response.data.results || [];
+    //console.log(searchResults);
+    setSearchResults(searchData);
+    //console.log(searchData[0].poster_path)
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getSearchMovies();
+    setInputValue("");
+  };
+
+  return (
+    <div className="search">
+      <div className="inputContainer">
+        <span className="inputIcon">
+          <AiOutlineSearch size={40} />
+        </span>
+        <form className="inputForm" onSubmit={handleSubmit}>
+          <input
+            className="inputText"
+            type="text"
+            placeholder="Search Movie..."
+            value={inputValue || ""}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      <div className="searchContainer">
+        {searchResults &&
+          searchResults.map((movie) => (
+            <Search props={movie} image={movie.backdrop_path} key={movie.id} />
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default SearchMovie;
