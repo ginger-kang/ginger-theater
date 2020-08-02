faed4687a1ed85badebab42490f91e8ccba65c4b
ginger-kang <kdhoon07@gmail.com>
2020-08-03 01:53:20 +0900

    Updates

---
+++ precache-manifest.1a05cb034e9b0c81b6d7b00029c283bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b998be89d9981ec3b05c8fab77e9ae5",
+    "url": "/ginger-theater/index.html"
+  },
+  {
+    "revision": "b06f59317a2d83b3a804",
+    "url": "/ginger-theater/static/css/main.90f32d9d.chunk.css"
+  },
+  {
+    "revision": "772d43a46cd78a67de94",
+    "url": "/ginger-theater/static/js/2.caa21772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ginger-theater/static/js/2.caa21772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06f59317a2d83b3a804",
+    "url": "/ginger-theater/static/js/main.103543e0.chunk.js"
+  },
+  {
+    "revision": "93bfd7cea9003bc94bfe",
+    "url": "/ginger-theater/static/js/runtime-main.c24dd759.js"
+  }
+]);
